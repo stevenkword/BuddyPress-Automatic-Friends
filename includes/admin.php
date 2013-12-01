35bee879cf5812af0bc1812b40206e6e5d5c84f9
Steven K Word <stevenword@gmail.com>
2013-12-01 15:31:37 -0500

    Adds donate link and button

--- includes/admin.php
+++ includes/admin.php
@@ -200,8 +200,15 @@ class s8d_BuddyPress_Automatic_Friends_Admin {
 						<div id="bpaf_display_optin" class="postbox ">
 							<h3 class="hndle"><span>Help Improve BPAF</span></h3>
 							<div class="inside">
-								<p>We would really appreciate your input to help us continue to improve the product. Find us at GitHub or donate using hte link below.</p>
-								<p><input type="button" value="Donate" id="bpaf-donate-button" class="button button-small"><br></p>
+								<p>We would really appreciate your input to help us continue to improve the product. Find us at GitHub or donate using the button below.</p>
+								<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+									<p style="width: 100%; text-align: center;">
+										<input type="hidden" name="cmd" value="_s-xclick">
+										<input type="hidden" name="hosted_button_id" value="DWK9EXNAHLZ42">
+										<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+										<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+									</p>
+								</form>
 							</div>
 						</div>
 						<div id="bpaf_display_contact" class="postbox ">
@@ -211,6 +218,7 @@ class s8d_BuddyPress_Automatic_Friends_Admin {
 								<ul class="bpaf_contacts">
 									<li><a href="http://wordpress.org/support/plugin/bp-automatic-friends" target="_blank"><span class="icon icon-wordpress"></span> BPAF Forum</a></li>
 									<li><a href="http://stevenword.com/plugins/bp-automatic-friends/" target="_blank"><span class="icon icon-plugin"></span> BPAF on the Web</a></li>
+									<li><a href="https://github.com/stevenkword/BuddyPress-Automatic-Friends" target="_blank"><span class="icon icon-wordpress"></span> BPAF on GitHub</a></li>
 									<li><a href="http://wordpress.org/support/view/plugin-reviews/bp-automatic-friends" target="_blank"><span class="icon icon-star "></span> Review BPAF on WordPress.org</a></li>
 								</ul>
 							</div>
